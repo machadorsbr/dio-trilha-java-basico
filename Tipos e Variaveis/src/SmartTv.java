4513488a49976b43f0ce392c3c5fff7d8f763a94
Guilherme <guilhermemacha@gmail.com>
2023-08-29 03:33:13 -0300

    primeiro exercicio concluido

---
+++ Tipos e Variaveis/src/SmartTv.java
@@ -0,0 +1,36 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void ligar() {
+        ligada = true;
+
+    };
+
+    public void desligar() {
+        ligada = false;
+
+    };
+
+    public void aumentaVolume() {
+        volume++;
+    }
+
+    public void diminuirVolume() {
+        volume--;
+    }
+
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void aumentaCanal() {
+        canal++;
+    }
+
+    public void diminuirCanal() {
+        canal--;
+    }
+
+}
